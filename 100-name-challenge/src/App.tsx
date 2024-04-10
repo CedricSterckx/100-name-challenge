f05e1a8833e8bc1869dd5943ea3adee0e2c683e3
Cédric Sterckx <cedric_sterckx@hotmail.com>
2024-04-10 23:53:19 +0200

    feat/fix game

--- 100-name-challenge/src/App.tsx
+++ 100-name-challenge/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type duplicateType = {
   isDuplicate: boolean,
@@ -8,25 +8,29 @@ type duplicateType = {
 export default function App() {
 
   const defaultIsDuplicate: duplicateType = { index: -1, isDuplicate: false };
-
   const [inputNames, setInputNames] = useState<string[]>([]);
   const [input, setInput] = useState<string>("");
   const [isDuplicate, setIsDucplicate] = useState<duplicateType>(defaultIsDuplicate);
   const [timeElapsed, setTimeElapsed] = useState(0);
-  const [timerActive, setTimerActive] = useState(false);
 
-  const endGameTreshold = 100;
+  //@ts-ignore
+  const intervalIdRef = useRef<NodeJS.Timeout | null>(null);
+  const lastItemRef = useRef(null);
+  const ENDGAMETHRESHOLD = 100;
 
-  let intervalId = 1;
 
+  useEffect(() => {
+    if (inputNames.length >= ENDGAMETHRESHOLD && intervalIdRef.current) {
+      clearInterval(intervalIdRef.current); // Stop the timer when the game ends
+    }
+  }, [inputNames.length, ENDGAMETHRESHOLD]);
 
   function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
     setIsDucplicate(defaultIsDuplicate);
-    if (inputNames.length >= endGameTreshold) {
-      clearInterval(intervalId);
+    if (inputNames.length >= ENDGAMETHRESHOLD) {
       return;
     }
-    if (!input) {
+    if (!input.trim()) {
       return;
     }
     if (e.key === 'Enter') {
@@ -41,19 +45,17 @@ export default function App() {
   };
 
   useEffect(() => {
-    if (timerActive) {
-      intervalId = setInterval(() => {
-        setTimeElapsed((prevTime) => prevTime + 1);
-      }, 1000);
-    } else {
-      clearInterval(intervalId);
+    if (lastItemRef.current) {
+      //@ts-ignore
+      lastItemRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-    return () => clearInterval(intervalId);
-  }, [timerActive]);
+  }, [inputNames]);
 
   function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
-    if (!timerActive) {
-      setTimerActive(true); // Start the timer when the user starts typing
+    if (!intervalIdRef.current) { // Start the timer on the first input
+      intervalIdRef.current = setInterval(() => {
+        setTimeElapsed(prevTime => prevTime + 1);
+      }, 1000);
     }
     setInput(e.target.value);
   }
@@ -65,23 +67,33 @@ export default function App() {
     return minutes > 0 ? `${minutes}m ${remainingSeconds.toString().padStart(2, '0')}` : `${remainingSeconds.toString().padStart(2, '0')}`;
   }
 
+  function resetGame() {
+    setInputNames([]);
+    setInput('');
+    setTimeElapsed(0);
+    if (intervalIdRef.current) clearInterval(intervalIdRef.current);
+    intervalIdRef.current = null;
+  }
+
   return (
     <main className="min-h-screen  bg-[#D7C6FC] p-4 flex flex-col justify-between items-center">
       <section className="flex flex-col gap-2">
-        <p className={`font-bold transition duration-1000 ${inputNames.length >= endGameTreshold ? 'scale-[2]' : ''}`}>Time Elapsed: {formatTime()} seconds</p>
+        <p className={`font-bold transition duration-1000 ${inputNames.length >= ENDGAMETHRESHOLD ? 'md:scale-[2]' : ''}`}>Time Elapsed: {formatTime()} seconds</p>
         <h1 className="text-xl">
           Your names:
         </h1>
         <ul className="max-h-[75vh] overflow-auto">
           {inputNames.map((name: string, index: number) => {
-            return <li key={`key${name} ${index}`} className={`${isDuplicate.index === index ? 'text-red-500 animate-shake' : ''} `}>
+            return <li
+              ref={index === inputNames.length - 1 ? lastItemRef : null}
+              key={`key${name} ${index}`} className={`${isDuplicate.index === index ? 'text-red-500 animate-shake' : ''} `}>
               {index + 1}) {name}
             </li>;
           })}
         </ul>
       </section >
       <section>
-        {inputNames.length < endGameTreshold ? (
+        {inputNames.length < ENDGAMETHRESHOLD ? (
           <div className="flex flex-col items-center">
             <input type="text" onChange={handleOnChange} value={input}
               onKeyUp={handleKeyUp} placeholder="Enter a name" className="p-2"
@@ -92,8 +104,9 @@ export default function App() {
             </div>
           </div>
         ) : (
-          <div>
+          <div className="flex flex-col">
             <span className="font-bold"> Goodjob - game is done !</span>
+            <button className="border p-2 bg-slate-200 border-sm shadow-md hover:shadow-xl" onClick={resetGame}>Reset game</button>
           </div>
         )}
       </section>
