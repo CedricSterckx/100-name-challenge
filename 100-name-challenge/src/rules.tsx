fd31590e970125b6e749b126b07da14bc02e7b1f
Cédric Sterckx <cedric_sterckx@hotmail.com>
2024-04-10 23:53:23 +0200

    feat/add rules

---
+++ 100-name-challenge/src/rules.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function rules() {
+    return (
+        <div>
+            <h1>Rules</h1>
+            <ul>
+                <ol>
+                    Cannot use help
+                </ol>
+                <ol>
+                    Can be a dead person
+                </ol>
+                <ol>
+                    Can be any version of their name (the rock vs Dwayne Johnson)
+                </ol>
+                <ol>
+                    Spelling doesn't count
+                </ol>
+                <ol>
+                    No family, friends or ex-girlfriend or neighbors or w/e. Can
+                    only be googleable person/ public figure
+                </ol>
+                <ol>
+                    No fictional character, must be someone with blood in their veins
+                </ol>
+                <ol>
+                    The timer start as soon as you start typing and will stop when you reach the 100 names
+                </ol>
+                <ol>
+                    Have to say the name, not a description (Michelle Obama is good, Obama's wife is bad)
+                </ol>
+            </ul>
+        </div>
+    );
+}
